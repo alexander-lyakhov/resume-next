0eda2830be9cc1e0ce8d98f767f3e47fb8f49285
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-01-31 12:25:48 +0200

    resume-next

---
+++ js/index.js
@@ -0,0 +1,14 @@
+﻿(function() {
+
+  const skills = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit.", "Ullam", "quam,", "dolor", "temporibus", "iste", "eos,", "hic", "dicta", "ex", "incidunt", "a", "minus", "sequi.", "Necessitatibus", "iste", "quisquam", "et", "repellendus", "labore", "illum", "porro", "sint", "accusantium,", "quasi"];
+  const $skills = document.querySelector('.content-block__skills ul');
+
+  const strSkills = skills.reduce((res, val) => {
+    return res += `<li>${val}</li>`
+  }, '')
+
+  $skills.innerHTML = strSkills
+
+  console.log(strSkills)
+
+})()
